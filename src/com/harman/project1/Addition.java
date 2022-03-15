c2f8337d56cd4d5757a0ac8e73d0b0f9fa804eee
pradeep2512m <pradeepm6383@gmail.com>
2022-03-15 11:03:29 +0530

    javapackages

---
+++ src/com/harman/project1/Addition.java
@@ -0,0 +1,9 @@
+package com.harman.project1;
+
+public class Addition {
+    public int Add_two_numbers(int a, int b)
+    {
+        Integer c=a+b;
+        return c;
+    }
+}
---
+++ src/com/harman/project1/Division.java
@@ -0,0 +1,9 @@
+package com.harman.project1;
+
+public class Division {
+    public int Div_two_numbers(int a, int b)
+    {
+        Integer c=a/b;
+        return c;
+    }
+}
